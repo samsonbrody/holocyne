381a6f9a0c894c20e8fb7c9d3531963b5c80a886
Samson <samsonbrody@gmail.com>
2022-06-15 11:48:48 -0700

    make navbar black

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,10 +6,10 @@ function Navbar() {
   const router = useRouter();
   return (
     <>
-      <div className="w-full font-Smooch bg-white h-24 sm:h-36 md:h-40 flex sm:flex-col items-center border-b justify-between sm:justify-center">
+      <div className="w-full font-Smooch bg-black h-24 sm:h-40 md:h-44 flex sm:flex-col items-center border-b justify-between sm:justify-center">
         <h1
           onClick={() => router.push("/")}
-          className="text-3xl sm:text-4xl md:text-5xl sm:ml-0 ml-6 cursor-pointer"
+          className="text-3xl sm:text-4xl md:text-5xl sm:ml-0 ml-6 cursor-pointer text-white"
         >
           Holocyne
         </h1>
@@ -17,19 +17,32 @@ function Navbar() {
           id="browsebar"
           className="hidden sm:flex mt-6 space-x-14 font-Lato"
         >
-          <h2 className="cursor-pointer hover:text-slate-700">Home</h2>
-          <h2 className="cursor-pointer hover:text-slate-700">Portfolio</h2>
-          <h2 className="cursor-pointer hover:text-slate-700">About</h2>
-          <h2 className="cursor-pointer hover:text-slate-700">Contact</h2>
+          <h2 className="cursor-pointer hover:text-slate-200 text-white">
+            Home
+          </h2>
+          <h2 className="cursor-pointer hover:text-slate-200 text-white">
+            Portfolio
+          </h2>
+          <h2 className="cursor-pointer hover:text-slate-200 text-white">
+            About
+          </h2>
+          <h2 className="cursor-pointer hover:text-slate-200 text-white">
+            Contact
+          </h2>
         </div>
         <div className="sm:hidden mr-6">
-          <Hamburger toggled={isOpen} toggle={setIsOpen} size={20} />
+          <Hamburger
+            color="white"
+            toggled={isOpen}
+            toggle={setIsOpen}
+            size={20}
+          />
         </div>
       </div>
       <div
         className={
           isOpen
-            ? `flex flex-col absolute sm:hidden items-center shadow-2xl py-6 gap-y-6 justify-center top-24 w-full bg-slate-900`
+            ? `flex flex-col absolute sm:hidden items-center shadow-2xl py-6 gap-y-6 justify-center top-[5.8rem] w-full bg-slate-900`
             : `hidden`
         }
       >
